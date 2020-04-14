d8f155442dae38f22c7baeae40a6a0b3a5c86841
Matthew Hudes <42047773+Matthewh19@users.noreply.github.com>
2020-04-14 13:07:33 -0400

    My work for today! I have been working with the sample app since it is easier to work with as I figure everything out.

--- Enhancement_Work/Sample Shiny/app.R
+++ Enhancement_Work/Sample Shiny/app.R
@@ -3,6 +3,10 @@ ui <- function(request) {
     textInput("txt", "Enter text"),
     checkboxInput("caps", "Capitalize"),
     verbatimTextOutput("out"),
+
+    actionButton("Load", "Load"),
+    selectInput("Select_settings", "Select which settings to use.", c("default")),
+    textInput("book_name", "Enter the name of your bookmark"),
     bookmarkButton()
   )
 }
@@ -13,9 +17,56 @@ server <- function(input, output, session) {
     else
       input$txt
   })
+
+  observeEvent(input$Load, {
+    list <- list.files(here::here("Enhancement_Work", "Sample Shiny", "shiny_bookmark_names"))
+    #list <- list.files(here::here("inst", "shiny", "shiny_bookmark_names"))
+    list <- stringr::str_replace(list, pattern = ".rds", replacement = "")
+    list <- unlist(list)
+
+
+    updateSelectInput(session, "Select_settings",
+                      choices = list)
+  })
+
+  onBookmarked(function(url) {
+    name <- paste(input$book_name, Sys.Date(), "bookmark.rds", sep = "_")
+    x <- list("name" = input$book_name, "url" = url)
+
+    saveRDS(x, file = here::here("Enhancement_Work", "Sample Shiny", "shiny_bookmark_names", name))
+    # saveRDS(x, file = here::here("inst", "shiny", "shiny_bookmark_names", name))
+
+    showNotification("The current state of QualtricsTools has been saved!", type = "message", duration = 7)
+
+    # store <- getShinyOption("bookmarkStore", default = "")
+    #
+    # subtitle <- "The current state of QualtricsTools has been saved."
+    #
+    # showModal(urlModal(url, subtitle = subtitle))
+  })
 }
 
 shinyApp(ui, server, enableBookmarking = "server")
 
 
-enableBookmarking
+#file <- readRDS(file = here::here("inst", "shiny", "shiny_bookmark_names", name))
+
+# list <- list.files(here::here("inst", "shiny", "shiny_bookmark_names"))
+#
+#   list <- list.files(here::here("Enhancement_Work", "Sample Shiny", "shiny_bookmark_names"))
+#   list <- stringr::str_replace(list, pattern = ".rds", replacement = "")
+#   list <- rlist::list.clean(list)
+#   list <- unlist(list)
+#  list
+#
+#    right <- c("husdf", "sagd")
+# #
+# #  typeof(list)
+# #  typeof(right)
+#  right
+#
+# length(list)
+# list[2]
+
+
+
